cdc4f911e91894316c42129385e9c728091bfcf3
= <omerfarukkpala@gmail.com>
2024-05-06 12:41:06 +0300

    api

---
+++ Portfolio/Core_Proje_Api/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using Core_Proje_Api.DAL.ApiContext;
+using Core_Proje_Api.DAL.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Proje_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            return Ok(context.Categories.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult CategoryGet(int id)
+        {
+            var value = context.Categories.Find(id);
+
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(value);
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category category)
+        {
+            context.Add(category);
+            context.SaveChanges();
+            return Created("", category); 
+        }
+
+        [HttpDelete]
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = context.Categories.Find(id);
+
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                context.Remove(value);
+                context.SaveChanges();
+                return NoContent();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult CategoryUpdate(Category category)
+        {
+            var value = context.Find<Category>(category.CategoryID);
+
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                value.CategoryName = category.CategoryName;
+                context.Update(value);
+                context.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ Portfolio/Core_Proje_Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,17 @@
+﻿using Core_Proje_Api.DAL.Entity;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Proje_Api.DAL.ApiContext
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = OMERFARUKPALA; database = CoreCvProjectApi; integrated security = true;");
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
