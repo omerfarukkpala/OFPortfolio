15cd285b79e0ae390546faf0c26be9630a94db40
= <omerfarukkpala@gmail.com>
2024-05-06 12:40:46 +0300

    Entity

---
+++ Portfolio/EntityLayer/Concrete/Portfolio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfolio
+    {
+        [Key]
+        public int PortfolioID { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public string ProjectUrlTitle { get; set; }
+        public string ProjectUrl { get; set; }
+        public string ImageUrl1 { get; set; }
+        public string ImageUrl2 { get; set; }
+        public string ImageUrl3 { get; set; }
+        public string ImageUrl4 { get; set; }
+        public string Price { get; set; }
+        public string Value { get; set; }
+        public bool Status { get; set; }
+    }
+}
